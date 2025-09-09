554f783aa46db68320576f90b313e1af2ab553f8
Scott <191410593+777Tu@users.noreply.github.com>
2025-09-09 17:14:27 +0100

    App.py

--- App.py
+++ App.py
@@ -13,7 +13,7 @@ def add_security_headers(response):
 
 @app.route("/")
 def home():
-    return "Hello, my secure website!"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
